ae379ccd6856becf5a95273c92976150125c1a63
Angelo Andrea Isola <andrea.isola@me.com>
2021-03-26 13:31:50 +0100

    stampa qualcosa nella bacheca

--- www/comunicationController.js
+++ www/comunicationController.js
@@ -5,182 +5,22 @@
 
     //function onDeviceReady() {
         
-        function register() {
-            $.ajax({
-                type: "POST",
-                url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "register.php",
-                data: JSON.stringify({
-
-                }),
-                success: function (data) {
-                    json = JSON.parse(data);
-                    console.log("The ajax request for %22register%22 succeeded!");
-                    console.log("The result is: ");
-                    console.dir(data);
-
-                    return json
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                }
-            })
-        }
-
-        function getProfile(sid) {
-            $.ajax({
-                type: "POST",
-                url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "getProfile.php",
-                data: JSON.stringify({
-                    "sid": sid
-                }),
-                success: function (data) {
-                    json = JSON.parse(data);
-                    console.log("The ajax request for %22getProfile%22 succeeded!");
-                    console.log("The result is: ");
-                    console.dir(data);
-
-                    return json
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                }
-            })
-        }
-
-        function addChannel(sid, nomeCanale) {
-            $.ajax({
-                type: "POST",
-                url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "addChannel.php",
-                data: JSON.stringify({
-                    "sid": sid,
-                    "ctitle": nomeCanale
-                }),
-                success: function (data) {
-                    json = JSON.parse(data);
-                    console.log("The ajax request for %22addChannel%22 succeeded!");
-                    console.log("The result is: ");
-                    console.dir(data);
-
-                    return json
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                }
-            })
-        }
-
-         export const getWall = (sid) => {
+         export const getWall = (sid, callback) => {
             var http = new XMLHttpRequest()
-
             $.ajax({
                 type: "POST",
                 url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "getWall.php",
                 data: JSON.stringify({
                     "sid": sid
                 }),
-                success: function (data) {
-                    var json = JSON.parse(data);
-                    var all_channels = json.channels;
-                    console.log("The ajax request for %22getWall%22 succeeded!");
-
-                    return json
-                    //console.log(all_channels);
-
-                    //TODO: SALVALA NEL MODEL
-
-                },
+                success: callback,
                 error: function (error) {
                     console.log(error.responseText);
                 }
             })
         }
 
-        function addPost(sid, nomeCanale, type, content, lat, lon) {
-            var http = new XMLHttpRequest()
-            $.ajax({
-                type: "POST",
-                url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "addPost.php",
-                data: JSON.stringify({
-                    "sid": sid,
-                    "ctitle": nomeCanale,
-                    "type": type,
-                    "content": content,
-                    "lat": lat,
-                    "lon": lon
-                }),
-                success: function (data) {
-                    console.log("The ajax request for %22addPost%22 succeeded!");
-                    return json
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                }
-            })
-        }
-
-        function getChannel(nomeCanale) {
-            $.ajax({
-                type: "POST",
-                url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "getChannel.php",
-                data: JSON.stringify({
-                    "sid": sid,
-                    "ctitle": nomeCanale
-                }),
-                success: function (data) {
-                    json = JSON.parse(data);
-                    elenco_posts = json.posts;
-                    console.log("The ajax request for %22getChannel%22 succeeded!");
-                    //console.log("The result is: ");
-                    //console.dir(data);
-                    return json
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                }
-            })
-        }
-
-        function getPostImage(sid, pid) {
-            var http = new XMLHttpRequest()
-            $.ajax({
-                type: "POST",
-                url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "getPostImage.php",
-                data: JSON.stringify({
-                    "sid": sid,
-                    "pid": pid
-                }),
-                success: function (data) {
-                    json = JSON.parse(data);
-                    console.log("The ajax request for %22getPostImagel%22 succeeded!");
-
-                    return json
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                }
-            })
-        }
-
-        function getUserPicture(sid, uid) {
-            var http = new XMLHttpRequest()
-            $.ajax({
-                type: "POST",
-                url: 'https://ewserver.di.unimi.it/mobicomp/accordo/' + "getUserPicture.php",
-                data: JSON.stringify({
-                    "sid": sid,
-                    "uid": uid
-                }),
-                success: function (data) {
-                    json = JSON.parse(data);
-                    console.log("The ajax request for %22getUserPicture%22 succeeded!");
-
-                    return json
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                }
-            })
-        }
+        
 
 
 
--- www/main.js
+++ www/main.js
@@ -5,6 +5,127 @@ import {getWall} from './comunicationController.js'
 var sid = "dDYkswaNkBtycWDS"
 var firstUse = true
 
+
+class Model {
+  constructor() {
+
+    this._channels = []
+
+    /*
+       this._channels = [
+          {ctitle: 'CANALE INSERISTO STATICAMENTE 1', mine: 'f'},
+          {ctitle: 'CANALE INSERISTO STATICAMENTE 2', mine: 't'},
+        ]
+    */
+
+        //console.log(_channels);
+  }
+
+  saveChannels = (response) => {
+    var json = JSON.parse(response);
+    var channels_list = json.channels;
+
+    channels_list.forEach(element => {
+      var channel = {
+        ctitle: element.ctitle,
+        mine: element.mine
+      }
+      this._channels.push(channel)   
+    });
+    console.log("All channles save into model");
+    console.log(this._channels);
+  }
+}
+
+
+class View {
+  constructor() {
+        // The root element
+        this.app = this.getElement('#root')
+
+        // The title of the app
+        this.title = this.createElement('h1')
+        this.title.textContent = 'Bacheca'
+    
+        // The form, with a [type="text"] input, and a submit button
+        this.form = this.createElement('form')
+    
+        this.input = this.createElement('input')
+        this.input.type = 'text'
+        this.input.placeholder = 'Add channel'
+        this.input.name = 'channel'
+    
+        this.submitButton = this.createElement('button')
+        this.submitButton.textContent = 'Submit'
+    
+        // The visual representation of the todo list
+        this.channelList = this.createElement('ul', 'channel-list')
+    
+        // Append the input and submit button to the form
+        this.form.append(this.input, this.submitButton)
+    
+        // Append the title, form, and todo list to the app
+        this.app.append(this.title, this.form, this.channelList)
+
+  }
+
+  displayTodos(_channels) {
+    // Delete all nodes
+while (this.channelList.firstChild) {
+  this.channelList.removeChild(this.channelList.firstChild)
+}
+
+// Show default message
+
+    // Create todo item nodes for each todo in state
+    _channels.forEach(channel => {
+      const li = this.createElement('li')
+      li.id = channel.ctitle
+  
+      // The todo item text will be in a contenteditable span
+      const span = this.createElement('span')
+      span.contentEditable = true
+      span.classList.add('editable')
+  
+  
+      // Append nodes to the todo list
+      this.channelList.append(li)
+    })
+  }
+
+  _resetInput() {
+    this.input.value = ''
+  }
+
+    // Create an element with an optional CSS class
+    createElement(tag, className) {
+      const element = document.createElement(tag)
+      if (className) element.classList.add(className)
+  
+      return element
+    }
+  
+    // Retrieve an element from the DOM
+    getElement(selector) {
+      const element = document.querySelector(selector)
+  
+      return element
+    }
+}
+
+class Controller {
+  constructor(model, view) {
+    this.model = model
+    this.view = view
+  }
+}
+
+console.log("1");
+const app = new Controller(new Model(), new View())
+console.log("2");
+
+
+
 window.onload = function () {
 
     document.addEventListener("deviceready", onDeviceReady, false);
@@ -19,25 +140,14 @@ window.onload = function () {
       }
     }
   
-    //CHIAMA LA WALL E LA MOSTRA
-      getWall(sid)
-      //TODO: printWall()
-  
-  
-    //CONTROLLA SE C'è IL CLICK
-    // locate your element and add the Click Event Listener
-    document.getElementById("parent-list").addEventListener("click", function (e) {
-      // e.target is our targetted element.
-      // try doing console.log(e.target.nodeName), it will result LI
-      if (e.target && e.target.nodeName == "LI") {
-        console.log(e.target.id + " was clicked");
-        channel_name = e.target.id.substring(3, e.target.id.length - 3);
-        console.log(channel_name + " was clicked");
-  
-        getChannel(channel_name)
-        showscreen("#screen_canale")
-  
-  
-      }
-    });
+    //CHIAMA LA WALL
+      var listaCanali = getWall(sid, (response) => {
+        console.log("Call %22getWall%22 succeded");
+        
+        //SALVA LISTA CANALI NEL MODEL
+        app.model.saveChannels(response)
+        app.view.displayTodos(app.model._channels) 
+
+    })
+    
   }
