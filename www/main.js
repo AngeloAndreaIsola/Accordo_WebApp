9e064571ee2ce4e8401d91e4c4e4cb462ca90463
Angelo Andrea Isola <andrea.isola@me.com>
2021-03-26 14:31:18 +0100

    Stampa tutti i canali nella bacheca

--- www/main.js
+++ www/main.js
@@ -1,5 +1,7 @@
 const base_url = "https://ewserver.di.unimi.it/mobicomp/accordo/"
-import {getWall} from './comunicationController.js'
+import {
+  getWall
+} from './comunicationController.js'
 
 
 var sid = "dDYkswaNkBtycWDS"
@@ -18,7 +20,7 @@ class Model {
         ]
     */
 
-        //console.log(_channels);
+    //console.log(_channels);
   }
 
   saveChannels = (response) => {
@@ -30,7 +32,7 @@ class Model {
         ctitle: element.ctitle,
         mine: element.mine
       }
-      this._channels.push(channel)   
+      this._channels.push(channel)
     });
     console.log("All channles save into model");
     console.log(this._channels);
@@ -40,56 +42,68 @@ class Model {
 
 class View {
   constructor() {
-        // The root element
-        this.app = this.getElement('#root')
+    // The root element
+    this.app = this.getElement('#root')
 
-        // The title of the app
-        this.title = this.createElement('h1')
-        this.title.textContent = 'Bacheca'
-    
-        // The form, with a [type="text"] input, and a submit button
-        this.form = this.createElement('form')
-    
-        this.input = this.createElement('input')
-        this.input.type = 'text'
-        this.input.placeholder = 'Add channel'
-        this.input.name = 'channel'
-    
-        this.submitButton = this.createElement('button')
-        this.submitButton.textContent = 'Submit'
-    
-        // The visual representation of the todo list
-        this.channelList = this.createElement('ul', 'channel-list')
-    
-        // Append the input and submit button to the form
-        this.form.append(this.input, this.submitButton)
-    
-        // Append the title, form, and todo list to the app
-        this.app.append(this.title, this.form, this.channelList)
+    // The title of the app
+    this.title = this.createElement('h1')
+    this.title.textContent = 'Bacheca'
+
+    // The form, with a [type="text"] input, and a submit button
+    this.form = this.createElement('form')
+
+    this.input = this.createElement('input')
+    this.input.type = 'text'
+    this.input.placeholder = 'Add channel'
+    this.input.name = 'channel'
+
+    this.submitButton = this.createElement('button')
+    this.submitButton.textContent = 'Submit'
+
+    // The visual representation of the todo list
+    this.channelList = this.createElement('ul', 'channel-list')
+
+    // Append the input and submit button to the form
+    this.form.append(this.input, this.submitButton)
+
+    // Append the title, form, and todo list to the app
+    this.app.append(this.title, this.form, this.channelList)
 
   }
 
   displayTodos(_channels) {
-    // Delete all nodes
-while (this.channelList.firstChild) {
-  this.channelList.removeChild(this.channelList.firstChild)
-}
+    console.log("_channels da displayTodos: "+_channels);
 
-// Show default message
+    var channels = [
+      {ctitle: 'CANALE INSERISTO STATICAMENTE 1', mine: 'f'},
+      {ctitle: 'CANALE INSERISTO STATICAMENTE 2', mine: 't'},
+    ]
+
+    // Delete all nodes
+    while (this.channelList.firstChild) {
+      this.channelList.removeChild(this.channelList.firstChild)
+    }
+
+    // Show default message
 
     // Create todo item nodes for each todo in state
-    _channels.forEach(channel => {
+    _channels.forEach(channel => {                         
       const li = this.createElement('li')
+      console.log("CHANNEL.CTITLE"+channel.ctitle);
       li.id = channel.ctitle
-  
+
       // The todo item text will be in a contenteditable span
       const span = this.createElement('span')
-      span.contentEditable = true
-      span.classList.add('editable')
-  
-  
+
+      // Display the title
+      span.textContent = channel.ctitle
+      
+
+      li.append(span);
+
       // Append nodes to the todo list
       this.channelList.append(li)
+
     })
   }
 
@@ -97,20 +111,20 @@ while (this.channelList.firstChild) {
     this.input.value = ''
   }
 
-    // Create an element with an optional CSS class
-    createElement(tag, className) {
-      const element = document.createElement(tag)
-      if (className) element.classList.add(className)
-  
-      return element
-    }
-  
-    // Retrieve an element from the DOM
-    getElement(selector) {
-      const element = document.querySelector(selector)
-  
-      return element
-    }
+  // Create an element with an optional CSS class
+  createElement(tag, className) {
+    const element = document.createElement(tag)
+    if (className) element.classList.add(className)
+
+    return element
+  }
+
+  // Retrieve an element from the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector)
+
+    return element
+  }
 }
 
 class Controller {
@@ -128,26 +142,26 @@ console.log("2");
 
 window.onload = function () {
 
-    document.addEventListener("deviceready", onDeviceReady, false);
-  
-    function onDeviceReady() {
-      // Now safe to use device APIs
-      
-      if (firstUse) {
-        //TODO: Registrazione implicità
-        //TODO: Inizializza db
-        firstUse = false
-      }
-    }
-  
-    //CHIAMA LA WALL
-      var listaCanali = getWall(sid, (response) => {
-        console.log("Call %22getWall%22 succeded");
-        
-        //SALVA LISTA CANALI NEL MODEL
-        app.model.saveChannels(response)
-        app.view.displayTodos(app.model._channels) 
+  document.addEventListener("deviceready", onDeviceReady, false);
 
-    })
-    
+  function onDeviceReady() {
+    // Now safe to use device APIs
+
+    if (firstUse) {
+      //TODO: Registrazione implicità
+      //TODO: Inizializza db
+      firstUse = false
+    }
   }
+
+  //CHIAMA LA WALL
+  var listaCanali = getWall(sid, (response) => {
+    console.log("Call %22getWall%22 succeded");
+
+    //SALVA LISTA CANALI NEL MODEL
+    app.model.saveChannels(response)
+    app.view.displayTodos(app.model._channels)
+
+  })
+
+}
