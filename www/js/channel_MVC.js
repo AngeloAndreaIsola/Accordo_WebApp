eca625437b3c6a8aaa7bb23f80702ec708cab17b
Angelo Andrea Isola <andrea.isola@me.com>
2021-04-24 14:35:26 +0200

    Aggiunto div mappa

--- www/js/channel_MVC.js
+++ www/js/channel_MVC.js
@@ -179,6 +179,10 @@ var sid = "dDYkswaNkBtycWDS"
 
     //handleClickOnCondividiImmgagine
 
+    //hanndleClickOnCondividiPosizione
+
     //handleClickOnImmagine
+
+    //handleClickOnBackToWall
 
   }
